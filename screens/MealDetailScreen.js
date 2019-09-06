c3ecd5a7686d131a633bd24f416b95003a117119
Jay T <jaytula@gmail.com>
2019-09-06 09:11:10 -0700

    Finish Lesson 106: Adding Screens

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealsDetailScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Meals Detail Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+export default MealsDetailScreen;
