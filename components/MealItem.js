3eec3d0a15497f5528104076dfa18e925430298a
Jay T <jaytula@gmail.com>
2019-09-07 00:12:57 -0700

    Lesson 123: Rendering a Meals List

---
+++ components/MealItem.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground
+} from "react-native";
+
+const MealItem = props => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={props.onSelectMeal}>
+        <View style={styles.gridItem}>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground
+              style={styles.bgImage}
+              source={{ uri: props.image }}
+            >
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {props.title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetails }}>
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity.toUpperCase()}</Text>
+            <Text>{props.affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 10,
+    overflow: "hidden",
+    marginVertical: 5
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    paddingHorizontal: 10,
+    paddingVertical: 5
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    color: "white",
+
+    fontSize: 22,
+
+    textAlign: "center"
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end"
+  },
+  mealRow: {
+    flexDirection: "row"
+  },
+  mealHeader: {
+    height: "85%"
+  },
+  mealDetails: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%"
+  }
+});
+
+export default MealItem;
